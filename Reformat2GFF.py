1cb9ecff97502e3f4f6f48ebf8af4c813e4a4585
j-parkim <chbukim@gmail.com>
2025-11-02 21:17:17 -0500

    second commit

--- Reformat2GFF.py
+++ Reformat2GFF.py
@@ -1,12 +1,8 @@
 from collections import defaultdict
-import re
-
 class ReformatGFF(object):
     """
     To reformat inconsistent GFF/GTF files into standardized GFF3.
-    
-    Includes format detection:
-    - Separator, assigner, quoting, and internal subformats.
+    using attribute detection from ParseGffinfo.detect_attr_format()
     """
 
     def __init__(self, filepath, delimiter="\t"):
@@ -16,6 +12,7 @@ class ReformatGFF(object):
             - delimiter (str): column delimiter (default = tab)
         """
         self.filepath = filepath
+        self.parser = ParseGFFinfo(filepath)
         self.delimiter = delimiter
         self.separator = None # placeholder. TBD
         self.assigner = None # placeholder. TBD
@@ -37,153 +34,139 @@ class ReformatGFF(object):
                     break
             return ex_lines
 
-    def detect_attr_format(self, max_lines=100, apply=True):
-        """
-        This is to detect likely format that attribute string is using.
-        
-        Args:
-            - max_lines (int) : number of lines to look at for format detection
-            - apply (bool) : if True, update self.separator, self.assigner, self.format
+    def detect_format(self, max_lines = 100):
+        """Wrapper for ParseGFFinfo.detect_attr_format"""
+        format_info = self.parser.detect_attr_format(max_lines=max_lines)
 
-        Returns:
-            - dictionary: detected likely format of attributes
-        """
-        sep_candidates = set()
-        assign_candidates = set()
-        any_quotes = False
-        subformats = set()
-        unknown_sep_count = 0
-        unknown_asgn_count = 0
-        bad_lines =[]
+        seps = format_info.get("Separators", set())
+        asgns= format_info.get("Assigners", set())
 
-        with open(self.filepath, 'r') as gff:
-                for i, l in enumerate(gff):
-                    if i > max_lines:
-                        break
-                    if l.startswith("#"):
-                        continue
-                    fields = l.rstrip().split(self.delimiter)
-                    if len(l.rstrip().split(self.delimiter)) < 9:
-                        print("Check the file. There's no attribute column.")
-                        continue # if there's no attribute column, skip
-                    attrs = fields[8].strip()
-                    if not attrs:
-                        continue
-
-                    # Check separator
-                    found_sep = False
-                    if ";" in attrs:
-                        sep_candidates.add(";")
-                        found_sep = True
-
-                    if "," in attrs:
-                        # comma is tricky. 
-                        # First, check if comma is found inside quotes (GTFlike)
-                        if re.search(r'"[^"]*,[^"]*"', attrs): 
-                            subformats.add("',' inside quoted value")
-                        
-                        # Second, check if GFF3 standard format, then likely comma is subseparator
-                        elif ";" in sep_candidates:
-                            if re.search(r';[^;]*,[^;]*;', attrs):
-                                subformats.add("',' inside values with separator';'")
-                        
-                        # Otherwise, assume comma as a separator, but adding comment in subformat
-                        else:
-                            sep_candidates.add(",")
-                            subformats.add("',' as main separator (PLEASE CONFIRM)")
-                        found_sep = True
-
-                    if "\t" in attrs:
-                        sep_candidates.add("\t")
-                        found_sep = True
-
-                    if not found_sep:
-                        unknown_sep_count += 1
-                        bad_lines.append(l.strip()) # to present example lines with unknown separator
-                    
-                    # Check assigner
-                    if "=" in attrs: # Standard GFF3
-                        assign_candidates.add("=")
-                    elif re.search(r'\w+\s+"[^"]+"', attrs): # standard GTF (e.g., gene_id "Gene1"
-                        assign_candidates.add(" ")
-                        any_quotes = True
-
-                    else: 
-                        unknown_asgn_count += 1
-                        bad_lines.append(l.strip()) # to present example lines with unknown assigner
-                    
-                    # Quotation mark 
-                    if '"' in attrs:
-                        any_quotes = True
-
-        quoting = "present" if any_quotes else "absent"
-
-        # Report unknowns 
-        if unknown_sep_count > 0:
-            print(f"!!! {unknown_sep_count} lines had no common separator(';', ',', or tab). Check the file.")
-        if unknown_asgn_count > 0:
-            print(f"!!! {unknown_asgn_count} lines had no common assigner('=' or space). Check the file.")
-
-        # Now, update the foundings.
-        if "=" in assign_candidates and " " not in assign_candidates:
-            self.format = "GFF3-like"
-            self.assigner = "="
-        elif " " in assign_candidates:
-            if any_quotes:
-                self.format = "GTF-like"
-                self.assigner = " "
-        else:
-            self.format = "Unknown"
-            self.assigner = ", and/or ".join(sorted(assign_candidates)) if assign_candidates else "?"
-            print(f"Check the file, Could not determine assigner - options: {self.assigner or 'none'}")
-
-        if ";" in sep_candidates:
+        # Decide separator
+        if ";" in seps:
             self.separator = ";"
-        elif "," in sep_candidates:
-            if any("inside" in sf for sf in subformats):
-                self.separator = ";"
-                print("',' found inside attribute values - using ';' as main separator.")
-            else:
-                self.separator = ","
-                print("Verify if ',' is the main separator. No semicolon found, treating ',' as main separator.")
-        elif "\t" in sep_candidates:
+        elif "," in seps:
+            self.separator = ","
+        elif "\t" in seps:
             self.separator = "\t"
         else:
-            self.separator = ", and/or ".join(sorted(sep_candidates)) if sep_candidates else "?"
-            print(f"!!!Check the file, Could not determine separator - options: {self.separator or 'none'}")
+            self.separator = next(iter(seps),";")
+            if seps:
+                print(f"!!!!Multiple/Unknown separators deteccted: {seps}. Using '{self.separator}'")
         
-        if not apply:
-            format_likely = self.format
-            assigner_likely = self.assigner
-            separator_likely = self.separator
+        # Decide assigner
+        if "=" in asgns:
+            self.assigner = "="
+        elif " " in asgns:
+            self.assigner = " "
         else:
-            format_likely = self.format
-            assigner_likely = self.assigner
-            separator_likely = self.separator
+            self.assigner = next(iter(asgns), "=")
+            if asgns:
+                print(f"!!!!Multiple/Unknown assigners deteccted: {asgns}. Using '{self.assigner}'")
 
-        # Print summary
-        print("\n====== Attribute Format Detection Summary ======")
-        print(f"Analyzed up to {max_lines} lines.")
-        print(f"Format                : {format_likely}")
-        print(f"Detected separator(s) : {', '.join(sep_candidates) or 'none'}")
-        print(f"Detected assigner(s)  : {', '.join(assign_candidates) or 'none'}")
-        print(f"Is there Quotes?      : {quoting}")
-        print(f"Subformats observed   : {', '.join(subformats) or 'none'}")
-        print(f"Lines with unknown format : {unknown_sep_count + unknown_asgn_count}")
-        if bad_lines:
-            print("Examples of unknown lines : ")
-            for ex in bad_lines:
-                print(f" {ex}")
-        print("==================================================\n")
         
-        result = {
-        "Format": self.format,
-        "Separators": sep_candidates,
-        "Assigners": assign_candidates,
-        "Quotes": any_quotes,
-        "Subformats": subformats,
-        "Unknown_count": unknown_sep_count + unknown_asgn_count,
-        "Unknown_examples": bad_lines,
-        }
+        self.format = format_info.get("Format","unknown")
 
-        return result
+        print("\n=== Format summary (ReformatGFF) ===")
+        print(f"Format detected     : {self.format}")
+        print(f"Separator chosen    : '{self.separator}'  (from {seps})")
+        print(f"Assigner chosen     : '{self.assigner}'  (from {asgns})")
+        print(f"Quoted attributes   : {format_info.get('Quotes', False)}")
+        print(f"Subformats observed : {format_info.get('Subformats', 'none')}")
+        print("====================================\n")
+
+        return format_info
+    
+    # Internal helper function when needed
+    def _ensure_valid_gff3_value(self, key, v):
+        """
+        Quote GFF3 attribute values only when necessary
+            e.g., product=glucose-1-phosphate adenylyltransferase large subunit
+                  -> product="glucose-1-phosphate adenylyltransferase large subunit"
+        Skip quoting for knwon list-like attributes        
+        """
+
+        listlike_keys = {"dbxref", "ontology_term", "is_a", "derives_from", "belongs_to"}
+
+        # quote text-like values
+        if key.lower() not in listlike_keys:
+            if any(symbol in v for symbol in [" ", ";", "=", ","]):
+                if not (v.startswith('"') and v.endswith('"')): # check if already quoted
+                    v = f'"{v}"'
+        return v
+
+
+    def reformat(self, tobe="GFF3", outfile = True):
+        """
+        Convert attributes to standardized GFF3 or GTF-like syntax.
+        Uses detected separator/assigner if available.
+
+        Args:
+            tobe (str): choose the final file to be either "GFF3" or "GTF" format. (default: GFF3)
+            outfile (Bool or str): generate file if true
+                            True(Default) - save as filename_standardized.gff3
+                            False - print to screen only
+                            str - save using given filename
+        """
+        if self.separator is None or self.assigner is None: # if user didn't run detect_format in the class 
+            print(">>>Detecting attribute format first...")
+            self.detect_format()
+
+        if tobe.lower() not in {"gff3","gtf"}:
+            raise ValueError("tobe argument must be 'GFF3' or 'GTF'")
+        
+        sep = self.separator or ";"
+        assigner = self.assigner or "="
+
+        out = (f"{self.filepath}_standardized.{tobe.lower()}" 
+               if outfile is True 
+               else outfile if isinstance(outfile, str)
+               else None)
+        
+        outfile_ = open(out, "w") if out else None
+
+        with open(self.filepath, "r") as infile:
+            for l in infile:
+                if l.startswith("#"):
+                    if outfile_:
+                        outfile_.write(l)
+                    else:
+                        print(l.strip())
+                    continue
+
+                fields = l.rstrip().split(self.delimiter)
+                if len(fields) < 9:
+                    continue
+
+                # Parse attribues using detected marks 
+                # If line has no assigner returns, just print
+                attrs = {}
+                for kv in fields[8].split(sep):
+                    kv = kv.strip()
+                    if not kv:
+                        continue
+                    if assigner in kv:
+                        k, v = kv.split(assigner,1)
+                        attrs[k.strip()] = v.strip()
+                    else:
+                        attrs[kv.strip()] = ""
+
+                # Convert to target syntax
+                if tobe.lower() == "gtf":
+                    new_attrs = "; ".join(f'{k} "{v}"' for k, v in attrs.items()) + ";"
+                else: # gff3
+                    valid_attrs = []
+                    for k,v in attrs.items():
+                        v = self._ensure_valid_gff3_value(k,v)
+                        valid_attrs.append(f"{k}={v}")
+                    new_attrs = ";".join(valid_attrs)
+                
+                if outfile_:
+                    outfile_.write("\t".join(fields[:8] + [new_attrs]) + "\n")
+                else:
+                    print("\t".join(fields[:8] + [new_attrs]))
+        
+        if outfile_:
+            outfile_.close()
+            print(f"Reformatted file written to: {out}")
+        else:
+            print(">>> Reformatting completed. See below.")
